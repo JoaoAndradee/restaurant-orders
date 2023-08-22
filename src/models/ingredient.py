5a5de63cf22eb90a49e3925d55c64ed367043081
joao <joao.dev94@gmail.com>
2023-08-22 01:13:18 -0300

    Requisito 01

--- src/models/ingredient.py
+++ src/models/ingredient.py
@@ -1,4 +1,3 @@
-# Inicio do projeto
 from enum import Enum
 
 
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,17 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction, Ingredient  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    cheese = Ingredient("quejo")
+    ham = Ingredient("presunto")
+    butter = Ingredient("manteiga")
+
+    assert ham.name == "presunto"
+    assert cheese.__hash__() == cheese.__hash__()
+    assert ham.__hash__() != butter.__hash__()
+    assert cheese == cheese
+    assert cheese != butter
+    assert repr(butter) == "Ingredient('manteiga')"
+    assert ham.restrictions == { Restriction.ANIMAL_DERIVED, Restriction.ANIMAL_MEAT }
+
